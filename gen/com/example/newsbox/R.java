249d4c0a57290d3b000f5f14b9d2a9205b333d95
MadhuriShah <madhurishah393@gmail.com>
2015-04-08 15:19:03 -0400

    populated listview static

--- gen/com/example/newsbox/R.java
+++ gen/com/example/newsbox/R.java
@@ -8,6 +8,9 @@
 package com.example.newsbox;
 
 public final class R {
+    public static final class array {
+        public static final int ListItem=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -25,13 +28,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LinearLayout1=0x7f090000;
+        public static final int action_settings=0x7f090002;
+        public static final int list1=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -60,10 +65,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
