83742d9dcad43fc07f7061ae2096d186ccabf774
MadhuriShah <madhurishah393@gmail.com>
2015-04-08 15:31:10 -0400

    Message to next activity on item click

--- gen/com/example/newsbox/R.java
+++ gen/com/example/newsbox/R.java
@@ -29,19 +29,24 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090000;
-        public static final int action_settings=0x7f090002;
+        public static final int action_settings=0x7f090004;
         public static final int list1=0x7f090001;
+        public static final int sample=0x7f090003;
+        public static final int textView2=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_news_list=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
+        public static final int news_list=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int title_activity_news_list=0x7f050003;
     }
     public static final class style {
         /** 
--- src/com/example/newsbox/MainActivity.java
+++ src/com/example/newsbox/MainActivity.java
@@ -1,17 +1,37 @@
 package com.example.newsbox;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
 
 public class MainActivity extends Activity {
-
+	private ListView mylistview1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mylistview1=(ListView)findViewById(R.id.list1);  
+        mylistview1.setOnItemClickListener(new OnItemClickListener() {
+        
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				 Intent i=new Intent(getApplicationContext(),NewsList.class);
+		            i.putExtra("UserId","Madhuri");  
+		            startActivity(i);
+				
+			}                
+        	
+		});
+        
+        
     }
 
 
