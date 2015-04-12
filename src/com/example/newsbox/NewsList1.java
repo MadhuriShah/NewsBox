4068d7f3bccfd260e85e6d28d486604061e8c45b
MadhuriShah <madhurishah393@gmail.com>
2015-04-12 16:34:46 -0400

    added url for world,education and lifestyle

--- src/com/example/newsbox/NewsList1.java
+++ src/com/example/newsbox/NewsList1.java
@@ -50,6 +50,12 @@ public class NewsList1 extends ListActivity {
 			 else if (category.equalsIgnoreCase("World")) {
 				 url="http://timesofindia.indiatimes.com/feeds/newsfeed/296589292.cms?feedtype=sjson";
 			 }
+			 else if (category.equalsIgnoreCase("Education")) {
+				 url="http://timesofindia.indiatimes.com/feeds/newsfeed/913168846.cms?feedtype=sjson";
+			 }
+			 else if (category.equalsIgnoreCase("Life and Style")) {
+				 url="http://timesofindia.indiatimes.com/feeds/newsfeed/2886714.cms?feedtype=sjson";
+			 }
 			//tv=(TextView)findViewById(R.id.textView2);
 			//tv.setText(UserId);
 			 mylistView2=getListView();	
