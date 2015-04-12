860d07a0eee2d14c86bb67f96324fe0e346d8e9b
MadhuriShah <madhurishah393@gmail.com>
2015-04-11 23:48:02 -0400

    added caption and removed byline

--- src/com/example/newsbox/NewsList1.java
+++ src/com/example/newsbox/NewsList1.java
@@ -26,7 +26,7 @@ public class NewsList1 extends ListActivity {
 	private static final String headline= "HeadLine";
 	//private static final String date = "DateLine";
 	private static final String story = "Story";
-	private static final String byline = "ByLine";
+	private static final String caption = "Caption";
 	JSONArray news;
 	ArrayList<HashMap<String, String>> catNews;
 		@Override
@@ -76,7 +76,7 @@ public class NewsList1 extends ListActivity {
 							JSONObject c = news.getJSONObject(i);
 							String story2 = c.getString(story);
 							String headline2= c.getString(headline);
-							String byline2=c.getString(byline);
+							String byline2=c.getString(caption);
 							// hashmap for singlen news
 							Log.d("hash", "map1");
 							HashMap<String, String> singleNews = new HashMap<String, String>();
@@ -84,7 +84,7 @@ public class NewsList1 extends ListActivity {
 							singleNews.put(headline, headline2);
 							Log.d("hash", "map2");
 							singleNews.put(story, story2);
-							singleNews.put(byline, byline2);
+							singleNews.put(caption, byline2);
 							catNews.add(singleNews);
 							Log.d("hash", "map3");
 				}
@@ -103,7 +103,7 @@ public class NewsList1 extends ListActivity {
 		super.onPostExecute(result);
 		// populating listview with json data
 		ListAdapter adapter = new SimpleAdapter(NewsList1.this, catNews,
-		R.layout.listitem, new String[] {headline,byline, story}, new int[] { R.id.headlineTextView,R.id.dateTextView,R.id.storyTextView });
+		R.layout.listitem, new String[] {headline,caption, story}, new int[] { R.id.headlineTextView,R.id.dateTextView,R.id.storyTextView });
 		setListAdapter(adapter);
 				
 			}
